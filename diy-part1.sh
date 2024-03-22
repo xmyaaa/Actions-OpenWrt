058be7bb65eac1b5373409ccaf6388eaae3c713e
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-22 17:47:44 +0800

    添加Passwall依赖feeds

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,7 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo '添加Passwall依赖feeds'
+# sed -i '1 i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-packages;main' feeds.conf.default
+echo '=========Add passwall feeds source OK!========='
