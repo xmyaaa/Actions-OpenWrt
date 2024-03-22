91847d3b1efeb2d750d1f9d5be4a37ec2881b3c8
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-22 17:49:46 +0800

    package/helloworld

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,6 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.co
 echo '添加Passwall依赖feeds'
 # sed -i '1 i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-packages;main' feeds.conf.default
 echo '=========Add passwall feeds source OK!========='
+
+rm -rf package/helloworld
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
