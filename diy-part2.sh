ad8a86bf7ee38e3a1ac76fb0fa34d92e1a38e78c
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-25 18:08:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,8 +28,8 @@ sed -i 's/socket" "iptables-mod-//g' feeds/luci/applications/luci-app-passwall/r
 echo '=========ALTER passwall denpendcies check OK!========='
 
 echo '修改主机名'
-sed -i "s/hostname='ImmortalWrt'/hostname='RAX3000M'/g" package/base-files/files/bin/config_generate
-cat package/base-files/files/bin/config_generate |grep hostname=
+#sed -i "s/hostname='ImmortalWrt'/hostname='RAX3000M'/g" package/base-files/files/bin/config_generate
+#cat package/base-files/files/bin/config_generate |grep hostname=
 echo '=========Alert hostname OK!========='
 
 echo '修改默认IP'
